ae4a731f48b9d2bf570f4eab28fe8b029de2a235
Ayush Gupta <91776561+AyushGupta74@users.noreply.github.com>
2023-12-07 15:41:46 +0530

    Day 1
    Basics of HTML and JAVASCRIPT

---
+++ Day 1/code/javascript/ex3.js
@@ -0,0 +1,56 @@
+x = 4
+
+// if(x%2 == 0)
+//   console.log(x," is even")
+// else
+//    console.log(x," is odd")
+
+// switch(x)
+// {
+//     case 1: console.log("one");break;
+//     case 2: console.log("two")
+//             break
+//     default : console.log("not 1 or 2")
+//               break;
+// }
+
+// let a=1
+// while(a++ < 10)
+//  {
+//     console.log(a)
+//  }
+
+// a=2
+// do{
+//  console.log(a++)
+// }while(a < 10)
+
+
+// for(let cnt=0;cnt<10;cnt++)
+// {
+//     console.log(cnt)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
